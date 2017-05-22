3813c26c1e3f1eeb3b29b7d09bcdbe2088f9cbef
Christopher Stock <email@christopherstock.de>
2017-05-22 14:59:34 +0200

    Setting up 1st example code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+    title = 'The 1st Angular App is working!';
+    name = 'Tester';
 }
